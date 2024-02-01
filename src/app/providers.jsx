e274b9278edb420ef2992d8c27c221d764db7f99
Sean Daly <sean.daly@thynkhealth.com>
2024-01-31 20:56:25 -0600

    Removed Dark theme for now

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -18,21 +18,7 @@ function ThemeWatcher() {
   let { resolvedTheme, setTheme } = useTheme()
 
   useEffect(() => {
-    let media = window.matchMedia('(prefers-color-scheme: dark)')
-
-    function onMediaChange() {
-      let systemTheme = media.matches ? 'dark' : 'light'
-      if (resolvedTheme === systemTheme) {
-        setTheme('system')
-      }
-    }
-
-    onMediaChange()
-    media.addEventListener('change', onMediaChange)
-
-    return () => {
-      media.removeEventListener('change', onMediaChange)
-    }
+    setTheme('light')
   }, [resolvedTheme, setTheme])
 
   return null
